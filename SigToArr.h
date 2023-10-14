eb8d310eeca48b646009a4a8d56e633a3bb14f14
Jasper1467 <Jasper1467@proton.me>
2023-10-14 13:49:20 +0200

    Update SigToArr.h

--- SigToArr.h
+++ SigToArr.h
@@ -1,36 +1,35 @@
 #pragma once
 
 #include <array>
-#include <cstdint>
 
 #ifdef SIGTOARR_USE_LOOKUP_TABLE
 namespace details
 {
-    namespace SigToArr
-    {
-        inline constexpr int ASCIIHexToInt[] =
-{
-    // ASCII
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-     0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,
-    -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	namespace SigToArr
+	{
+		inline constexpr int ASCIIHexToInt[] =
+		{
+			// ASCII
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,
+			-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 
-    // 0x80-FF (Omit this if you don't need to check for non-ASCII)
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-    -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
-};
-    } // namespace SigToArr
+			// 0x80-FF (Omit this if you don't need to check for non-ASCII)
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+			-2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2, -2,
+		};
+	} // namespace SigToArr
 
 } // details
 #endif
@@ -38,29 +37,28 @@ namespace details
 template <char... Chars>
 class CSigToArr
 {
-private:
-    template <char... Chars>
-    struct HexStringToIntegerArray
-    {
-        static constexpr std::array<int, sizeof...(Chars)> m_arrValue = {HexStringToInteger<Chars>::m_nValue...};
-    };
-
-    template <char Char>
-    struct HexStringToInteger
-    {
+	template <unsigned char Char>
+	struct HexStringToInteger
+	{
 #ifdef SIGTOARR_USE_LOOKUP_TABLE
-        static constexpr int m_nValue = details::SigToArr::ASCIIHexToInter[Char];
+		static constexpr int m_nValue = details::SigToArr::ASCIIHexToInt[static_cast<int>(Char)];
 #else
-        static constexpr int m_nValue =
-            (Char == '?') ? -1 : ((Char >= '0' && Char <= '9') ? (Char - '0') : (10 + (Char & 0x0F)));
+		static constexpr int m_nValue =
+			(Char == '?') ? -1 : ((Char >= '0' && Char <= '9') ? (Char - '0') : (10 + (Char & 0x0F)));
 #endif
-    };
+	};
+
+	template <char... Ch>
+	struct HexStringToIntegerArray
+	{
+		static constexpr std::array<int, sizeof...(Ch)> m_arrValue = { HexStringToInteger<Ch>::m_nValue... };
+	};
 
 public:
-    static constexpr std::array<int, sizeof...(Chars)> m_arrValue = {HexStringToIntegerArray<Chars>::m_arrValue...};
+	static constexpr std::array<int, sizeof...(Chars)> m_arrValue = { HexStringToIntegerArray<Chars>::m_arrValue... };
 
-    constexpr auto Get()
-    {
-        return m_arrValue;
-    }
+	constexpr auto Get() const
+	{
+		return m_arrValue;
+	}
 };
